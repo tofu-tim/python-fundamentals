08d0018a04ed13df4d84518322e2696fc03b7238
Timothy Wilger <47797775+tofu-tim@users.noreply.github.com>
2023-07-06 22:14:24 -0500

    Add files via upload

--- for_loops_basic_1.py
+++ for_loops_basic_1.py
@@ -2,6 +2,9 @@
 for x in range(151):
     print(x)
 
+print('Prediction: prints number 1-151')
+# prints numbers 1-151
+
 #multiples of 5
 for x in range(5, 1001, 5):
     print(x)
@@ -15,6 +18,8 @@ for x in range(101):
     else:
         print(x)
 
+print("Prediction: alternates between printing 'dojo' and 'coding dojo' 10x each.")
+
 # Whoa, that sucker's huge
 for x in range(50000):
     odd_sum = 0
@@ -22,9 +27,13 @@ for x in range(50000):
         odd_sum += x
 print(odd_sum)
 
+print("prediction: sums all odd numbers from 0-50000 and prints the result.")
+
 # Countdown by fours
 for x in range(2018, 0, -4):
     print(x)
+
+print("Prediction: prints a countdown from 2018-0. Counts down by four.")
 
 # Flexible counter
 lowNum = 1
@@ -34,3 +43,4 @@ mult = 2
 for x in range(lowNum, highNum, mult):
     print(x)
 
+print("Prediction: prints all odd numbers from 1-50.")
